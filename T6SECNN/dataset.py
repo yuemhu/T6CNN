dc38cfe6366736eede78dda2308348c313a4cce2
yuemhu <100929346+yuemhu@users.noreply.github.com>
2023-05-07 23:17:43 +0800

    T6CNN model script

---
+++ T6SECNN/dataset.py
@@ -0,0 +1,53 @@
+import numpy as np
+import torch
+from Bio import SeqIO
+from torch.utils.data import Dataset
+def openfasta(fasta_file):
+    seq_name = []
+    for record in SeqIO.parse(fasta_file, "fasta"):
+        description = record.description.split("|")
+        if description[0]=="gi":
+            name=description[3]
+        elif description[0]=="sp":
+            name=description[1]
+        else:
+            name=description[0]
+        seq_name.append(name)
+    return seq_name
+########k折划分############        
+def load_data(T6SE,T6SEfasta,prefix):  ###此过程主要是步骤（1）
+    if prefix == "ps100" or prefix == "bpb100" :
+        T6SE_file =np.loadtxt(T6SE,delimiter=',',dtype=str)
+        names = T6SE_file[:,0]
+        T6SE_file = T6SE_file[:,1:].astype(np.float32)
+    elif prefix == "AAC" or prefix == "DPC" or prefix == "TPC":
+        T6SE_file =np.loadtxt(T6SE,delimiter='\t',dtype=str,skiprows=1)
+        names = T6SE_file[:,0]
+        T6SE_file = T6SE_file[:,1:].astype(np.float32)
+    else:
+        T6SE_file =np.loadtxt(T6SE,delimiter=',',dtype=np.float32)
+        names = openfasta(T6SEfasta)
+        names = np.array(names)
+    T6SE_file[np.isposinf(T6SE_file)] = 9999
+    T6SE_file[np.isneginf(T6SE_file)] = -9999
+    T6SE_file[np.isnan(T6SE_file)] = 0 
+
+    return T6SE_file,names
+
+##########定义dataset##########
+class TestdataSet(Dataset):
+    def __init__(self,train_features,train_labels):
+        self.x_data = train_features
+        if train_labels is not None:
+            self.y_data = train_labels
+        else:
+            self.y_data = None
+        self.len = len(train_features)
+    
+    def __getitem__(self,index):
+        if self.y_data is not None:
+            return self.x_data[index],self.y_data[index]
+        else:
+            return self.x_data[index]
+    def __len__(self):
+        return self.len
---
+++ T6SECNN/model.py
@@ -0,0 +1,97 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+######网络结构##########
+class T6CNN_single(nn.Module):
+    #定义T6CNN
+    def __init__(self,in_dim: int):
+        super(T6CNN_single, self).__init__() 
+        self.in_dim = in_dim
+        self.max_dim = 10
+        if self.in_dim >800:
+            self.max_dim = 100
+            self.in_dim = self.in_dim//10
+            
+        self.conv1   = nn.Sequential(nn.Conv1d(in_channels=1,out_channels=10,kernel_size=3,padding=1),
+                        nn.BatchNorm1d(10),
+                        nn.ReLU(),
+                        nn.Dropout(0.20),
+                        nn.MaxPool1d(self.max_dim)
+            )
+        self.conv2   = nn.Sequential(nn.Conv1d(in_channels=1,out_channels=10,kernel_size=6,padding=3),
+                        nn.BatchNorm1d(10),
+                        nn.ReLU(),
+                        nn.Dropout(0.25),
+                        nn.MaxPool1d(self.max_dim)
+            )
+        self.conv3   = nn.Sequential(nn.Conv1d(in_channels=1,out_channels=10,kernel_size=9,padding=9//2),
+                        nn.BatchNorm1d(10),
+                        nn.ReLU(),
+                        nn.Dropout(0.35),
+                        nn.MaxPool1d(self.max_dim)
+            )
+        self.mlp = nn.Sequential(nn.Flatten(),
+                        nn.Linear(self.in_dim*3, 64),
+                        nn.ReLU(),
+                        nn.Dropout(0.35),
+                        nn.Linear(64, 2))
+  
+    def forward(self, x):
+        x = x.unsqueeze(1)
+
+        x1 = self.conv1(x)
+        x2 = self.conv2(x)
+        x3 = self.conv3(x)
+        x = self.mlp(torch.cat([x1,x2,x3],1))
+        return x
+class T6CNN(nn.Module):
+    #定义T6CNN
+    def __init__(self,in_dim: int):
+        super(T6CNN, self).__init__() 
+        self.in_dim = in_dim
+        if self.in_dim >800:
+            self.in_dim = 800
+        self.conv1   = nn.Sequential(nn.Conv1d(in_channels=1,out_channels=10,kernel_size=3,padding=1),
+                        nn.BatchNorm1d(10),
+                        nn.ReLU(),
+                        nn.Dropout(0.25),
+                        nn.Conv1d(in_channels=10,out_channels=self.in_dim,kernel_size=3,padding=1),
+                        nn.BatchNorm1d(self.in_dim),
+                        nn.ReLU(),
+                        nn.Dropout(0.25),
+                        nn.MaxPool1d(in_dim)
+            )
+        self.conv2   = nn.Sequential(nn.Conv1d(in_channels=1,out_channels=10,kernel_size=6,padding=3),
+                        nn.BatchNorm1d(10),
+                        nn.ReLU(),
+                        nn.Dropout(0.25),
+                        nn.Conv1d(in_channels=10,out_channels=self.in_dim,kernel_size=6,padding=3),
+                        nn.BatchNorm1d(self.in_dim),
+                        nn.ReLU(),
+                        nn.Dropout(0.25),
+                        nn.MaxPool1d(in_dim)
+            )
+        self.conv3   = nn.Sequential(nn.Conv1d(in_channels=1,out_channels=10,kernel_size=9,padding=9//2),
+                        nn.BatchNorm1d(10),
+                        nn.ReLU(),
+                        nn.Dropout(0.25),
+                        nn.Conv1d(in_channels=10,out_channels=self.in_dim,kernel_size=9,padding=9//2),
+                        nn.BatchNorm1d(self.in_dim),
+                        nn.ReLU(),
+                        nn.Dropout(0.25),
+                        nn.MaxPool1d(in_dim)
+            )
+        self.mlp = nn.Sequential(nn.Flatten(),
+                        nn.Linear(self.in_dim*3, 64),
+                        nn.ReLU(),
+                        nn.Dropout(0.25),
+                        nn.Linear(64, 2))
+  
+    def forward(self, x):
+        x = x.unsqueeze(1)
+
+        x1 = self.conv1(x)
+        x2 = self.conv2(x)
+        x3 = self.conv3(x)
+        x = self.mlp(torch.cat([x1,x2,x3],1))
+        return x
